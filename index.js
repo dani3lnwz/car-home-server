d83d1c26276a9ce2f7348c37fb42e70fb504311e
danielNewaz <danielnwz420@gmail.com>
2022-06-01 19:08:20 -0700

    cors problem

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({origin: 'https://all-in-one-95e03.web.app/'}))
 app.use(express.json())
 
 const uri = "mongodb+srv://dbUser1:test1234@cluster0.7rfww.mongodb.net/?retryWrites=true&w=majority";
